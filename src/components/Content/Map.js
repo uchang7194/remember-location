5345e9e29bae375b07ccd74b01b11d4004fb2824
uchang7194 <suyoung7194@gmail.com>
2017-12-09 13:21:25 +0900

    소스 정리

--- src/components/Content/Map.js
+++ src/components/Content/Map.js
@@ -31,6 +31,18 @@ export default class Map extends Component {
     this._handleMarkersData = this._handleMarkersData.bind(this);
     this._handleDeleteMarker = this._handleDeleteMarker.bind(this);
   }
+
+  
+  /**
+   *
+   * init methods
+   *
+   */
+  
+  /**
+   * @method _initMap
+   * @description google 맵 초기화
+   */
   _initMap = () => {
     const google = window.google;
     this.map = new google.maps.Map(document.getElementById('map'), {
@@ -46,6 +58,10 @@ export default class Map extends Component {
     this._initInfoWindow(google, this.map);
     // this._handleAddEvent(map, 'click', this._callbackMapTest);
   }
+  /**
+   * @method _initSearch
+   * @description google search api 설정
+   */ 
   _initSearch = (google, map) => {
     
     let input = this.refs.searchAddr,
@@ -78,6 +94,10 @@ export default class Map extends Component {
 
     this.info_window = _info_window;
   }
+  /**
+   * @method _handleResizeMap
+   * @description 브라우저의 높이가 변경될 때 map의 height를 resize해주는 메서드
+   */ 
   _handleResizeMap = () => {
     let _map = null;
     const _browser_height = window.innerHeight;
@@ -92,6 +112,18 @@ export default class Map extends Component {
       });
     }
   }
+
+  
+  /**
+   *
+   * Marker methods
+   *
+   */
+  
+  /**
+   * @method _moveTo 
+   * @description marker가 google map에 표시되었을 때 이동하는 메서드
+   */ 
   _moveTo = (position, marker_addr) => {
 
     // if( this._isContainedLocData(position) ) {
@@ -102,6 +134,16 @@ export default class Map extends Component {
     this.map.setZoom(15);
     this._addMarker(position, marker_addr);
   }
+  /**
+   * @method _addMarker
+   * @description 마커를 추가시키는 메서드
+   * @param position lat lng 키값이 들어있는 Object
+   * @param marker_addr 위치 주소 String
+   * @param marker_tit 제목 String
+   * @param marker_des 내용 String
+   * @param isSaved 저장된 마커정보인지 판별하기 위한 값 Boolean
+   * @param isContainedMarkerChk 이미 포함되어있는 마커정보인지 확인 유무를 위한 값 Boolean
+   */ 
   _addMarker = (position, marker_addr = '', marker_tit = '' , marker_des = '', isSaved = false, isContainedMarkerChk = true) => {
     const google_map = window.google.maps;
     let marker = null;
@@ -138,6 +180,12 @@ export default class Map extends Component {
     this.markerCluster.addMarker(marker);
     
   }
+  /**
+   * @method _markerOnClick
+   * @description 마커가 추가 되었을 때 이벤트를 적용시켜주는 함수
+   * @memberOf _addMarker
+   * @param marker 마커 객체 Object
+   */ 
   _markerOnClick = (marker) => {
     marker.addListener('click', () => {
 
@@ -173,6 +221,12 @@ export default class Map extends Component {
       });
     });
   }
+  /**
+   * @method _isLatLngFunc
+   * @description 위치 정보를 가지고 있는 객체의 lat, lng이 함수인지 아닌지 판별하는 메서드
+   * @memberOf _getPosition
+   * @param position Object
+   */ 
   _isLatLngFunc = (position) => {
     if( typeof position.lat === 'function' ) {
       return true;
@@ -180,6 +234,11 @@ export default class Map extends Component {
       return false;
     }
   }
+  /**
+   * @method _getPosition
+   * @description _isLatLngFunc함수로 함수인지 판별하고 맞으면 함수를 호출해 lat, lng값을 객체형태로 반환하는 메서드
+   * @param position Object
+   */ 
   _getPosition = (position) => {
     if( this._isLatLngFunc(position) ) {
       return {
@@ -193,6 +252,12 @@ export default class Map extends Component {
       };
     }
   }
+  /**
+   * @method _isContainedMarker
+   * @description 마커가 포함되어 있는지 확인하는 메서드, 위치정보를 비교해서 판별
+   * @memberOf _addMarker
+   * @param position
+   */ 
   _isContainedMarker = (position) => {
     const compare_marker_pos = this._getPosition(position);
     let _marker = null;
@@ -212,6 +277,10 @@ export default class Map extends Component {
 
     return _marker;
   }
+  /**
+   * @method _clearMarkers
+   * @description this.markers의 마커 객체를 setMap을 사용해서 맵에서 지우고 state의 markersData를 초기화 시켜주는 메서드
+   */ 
   _clearMarkers = () => {
     const _markers = this.markers, length = _markers.length;
     let i = 0;
@@ -223,7 +292,12 @@ export default class Map extends Component {
     }
     this.markers = [];
   }
-  
+  /**
+   * @method _handleMarkersData
+   * @description ModalLocInfo component에 props로 넘겨주는 메서드 / 데이터를 저장, 수정하는 역할을 함.
+   * @param data 위치정보 Object
+   * @param is_modified 수정할것인지 아닌지 판별하는 값 Boolean
+   */
   _handleMarkersData = (data, is_modified = false) => {
 
     if( !window.confirm('이대로 저장하시겠습니까?') ) {
@@ -273,6 +347,11 @@ export default class Map extends Component {
     });
   }
   
+  /**
+   * @method _handleDeleteMarker
+   * @description ModalLocInfo component에 props로 보내는 메서드 / 마커, 마커정보를 삭제하기위해 사용
+   * @param position 위치정보(lat, lng) Object
+   */ 
   _handleDeleteMarker = (position) => {
 
     if( !window.confirm('삭제하시겠습니까?') ) {
@@ -326,6 +405,19 @@ export default class Map extends Component {
       }
     });
   }
+
+
+  
+  /**
+   *
+   * toggle methods
+   *
+   */
+  
+  /**
+   * @method _handleToggleActivedModal
+   * @description ModalLocInfo component를 토글시켜주기 위한 메서드
+   */ 
   _handleToggleActivedModal = () => {
     this.setState({
       activedModal: !this.state.activedModal
@@ -333,8 +425,18 @@ export default class Map extends Component {
       this._renderMarkers();
     });
   }
-  // geolocation으로 좌표값 구하기
-
+  
+  
+  /**
+   *
+   * Geolocation methods
+   *
+   */
+  
+  /**
+   * @method _handleGeoLocation
+   * @description 현재 위치 정보를 받기 위한 메서드
+   */ 
   _handleGeoLocation = () => {
     const geolocation = navigator.geolocation;
 
@@ -344,6 +446,11 @@ export default class Map extends Component {
       }, this._handleErrorGeoLoc);
     }
   }
+  /**
+   * @method _handleSuccessGeoLoc
+   * @description _handleGeoLocation을 호출하고 성공적으로 위치가 받아졌을 때 처리하는 callback 함수
+   * @param position
+   */ 
   _handleSuccessGeoLoc = (position) => {
     /*
       coords
@@ -364,59 +471,39 @@ export default class Map extends Component {
       this._moveTo(this.state.current_coords);
     });
   }
+  /**
+   * @method _handleErrorGeoLoc
+   * @description _handleGeoLocation을 호출하고 위치가 받아지지 않았을 때(권한 미적용) 처리하는 callback 함수
+   */ 
   _handleErrorGeoLoc = () => {
     alert('위치 권한을 허용해주세요.');
   }
-  _renderModal = () => {
-    if( this.state.activedModal ) {
-      return (
-        <ModalLocInfo 
-          handleMarkersData={this._handleMarkersData}
-          handleDeleteMarker={this._handleDeleteMarker}
-          handleToggleModal={this._handleToggleActivedModal}
-          markerInfo={this.state.clickedMarkerInfo}
-          />
-      );
-    }
-  }
-  _renderMarkers = () => {
-    let delete_data = [];
-
-
-    if( this.markers.length === 0 ) { return; }
-
-    // this._clearMarkers();
-
-    this.markers.forEach( (data, index) => {
-      
-      if( !data.isSaved ) {
-        data.setMap(null);
-        delete_data.push(index);
-      }
-    });
-
-    delete_data.forEach( data => {
-      this.markers.splice(data, 1);
-    });
-    // this.markerCluster = null;
-    this.markerCluster.clearMarkers();
-    this.markerCluster.addMarkers(this.markers);
-    console.log('next this.markers: ', this.markers);
-  }
 
   
-
-  componentDidMount() {
-    this._initMap();
-    this._handleResizeMap();
-    window.addEventListener('resize', this._handleResizeMap);
-  }
-
+  /**
+   *
+   * firebase methos
+   *
+   */
+  
+  /**
+   * @method _setFirebaseData
+   * @description firebase의 데이터베이스에 데이터를 업데이트 시켜주는 메서드
+   * @param _url api 주소 String
+   * @param data markersData Array
+   */  
   _setFirebaseData = (_url, data) => {
     const URL = this.URL + '/' + _url;
 
     database.ref(URL).set(JSON.stringify(data));
   }
+  /**
+   * @method _initFirebaseData
+   * @description Facebook userID를 기준으로 firebase에 해당 userID가 없을 때 firebase에 api를 초기화 시켜주는 메서드
+   * @param _user_info 유저정보(Facebook) Object
+   * @param URL firebase url String
+   * @param KEY firebase에 추가할 때 사용할 KEY값 String
+   */ 
   _initFirebaseData = (_user_info, URL, KEY) => {
     const INIT_MARKER_DATA = {
       uid: _user_info.auth.userID,
@@ -432,6 +519,14 @@ export default class Map extends Component {
     
     database.ref().update(updates);
   }
+  /**
+   * @method _findFirebaseData
+   * @description Facebook의 userID가 firebase에 있는지 확인하는 메서드
+   * @param data firebase 전체 데이터 Object
+   * @param _uid Facebook userID String
+   * @return data[props] 해당 userID의 데이터 정보
+   * @return null 해당 userID의 데이터 정보가 없을 때 반환
+   */ 
   _findFirebaseData = (data, _uid) => {
 
     console.log('_uid: ', _uid);
@@ -446,6 +541,11 @@ export default class Map extends Component {
 
     return null;
   }
+  /**
+   * @method _fetchData
+   * @description firebase에서 데이터를 가져오는 메서드
+   * @param user_info Facebook 유저정보 Object
+   */ 
   _fetchData = (user_info) => {
     console.log('_fetchData user_info: ', user_info)
     // firebase
@@ -484,6 +584,81 @@ export default class Map extends Component {
     });
   }
   
+  
+  /**
+   *
+   * render methods
+   *
+   */
+  
+
+   /**
+   * @method _renderMarkers
+   * @description 마커의 isSaved 정보를 기준으로 true면 google map에 표시되게 하고 false면 this.markers와 this.state.markersData에서 지워주는 메서드
+   */ 
+  _renderMarkers = () => {
+    let delete_data = [];
+
+
+    if( this.markers.length === 0 ) { return; }
+
+    // this._clearMarkers();
+
+    this.markers.forEach( (data, index) => {
+      
+      if( !data.isSaved ) {
+        data.setMap(null);
+        delete_data.push(index);
+      }
+    });
+
+    delete_data.forEach( data => {
+      this.markers.splice(data, 1);
+    });
+    // this.markerCluster = null;
+    this.markerCluster.clearMarkers();
+    this.markerCluster.addMarkers(this.markers);
+    console.log('next this.markers: ', this.markers);
+  }
+
+  /**
+   * @method _renderModal
+   * @description ModalLocInfo component를 렌더해주는 메서드
+   */ 
+  _renderModal = () => {
+    if( this.state.activedModal ) {
+      return (
+        <ModalLocInfo 
+          handleMarkersData={this._handleMarkersData}
+          handleDeleteMarker={this._handleDeleteMarker}
+          handleToggleModal={this._handleToggleActivedModal}
+          markerInfo={this.state.clickedMarkerInfo}
+          />
+      );
+    }
+  }
+   
+  /**
+   * @method _renderLoading
+   * @description 데이터가 불러오는 동안 Content에 표시될 영역을 렌더링하는 메서드
+   */ 
+  _renderLoading = () => {
+    if( this.state.isLoading ) {
+      return (
+        <div className="content-loading">
+          Loading...
+        </div>   
+      );
+    }
+  }
+  
+  /**
+   *
+   * LifeCycle
+   *
+   */
+  
+  
   componentWillReceiveProps = (nextProps) => {
     // this._initFirebaseData(nextProps);
     const user_info = nextProps.userInfo;
@@ -511,16 +686,10 @@ export default class Map extends Component {
       });
     }
   }
-  
-  
-  _renderLoading = () => {
-    if( this.state.isLoading ) {
-      return (
-        <div className="content-loading">
-          Loading...
-        </div>   
-      );
-    }
+  componentDidMount() {
+    this._initMap();
+    this._handleResizeMap();
+    window.addEventListener('resize', this._handleResizeMap);
   }
   render() {
   
